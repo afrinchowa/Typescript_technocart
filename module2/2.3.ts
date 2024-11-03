97b72db7acdc7ed3756849ed925c9d87e35e0461
chowa <arfinchowa524@gmail.com>
2024-11-03 19:34:46 +0600

    2.3 added

--- module2/2.3.ts
+++ module2/2.3.ts
@@ -1,28 +1,35 @@
 {
-    // generic type
-    type GenericArray<param> = Array<param>
-    // const rollNumbers: number[] = [3,6,8];
-    const rollNumbers: GenericArray<number>= [3,6,8];
+  // generic type
+  type GenericArray<param> = Array<param>;
+  // const rollNumbers: number[] = [3,6,8];
+  const rollNumbers: GenericArray<number> = [3, 6, 8];
 
-    // const mentrors: string[] = ['Mr. X','Mr. Y','Mr. Z']
-    // const mentrors: Array<string> = ['Mr. X','Mr. Y','Mr. Z']
-    const mentrors: GenericArray<string> = ['Mr. X','Mr. Y','Mr. Z']
+  // const mentrors: string[] = ['Mr. X','Mr. Y','Mr. Z']
+  // const mentrors: Array<string> = ['Mr. X','Mr. Y','Mr. Z']
+  const mentrors: GenericArray<string> = ["Mr. X", "Mr. Y", "Mr. Z"];
 
-    // const boolArray: boolean[] = [true,false,true]
-    // const boolArray: Array<boolean> = [true,false,true]
-    const boolArray: GenericArray<boolean> = [true,false,true]
+  // const boolArray: boolean[] = [true,false,true]
+  // const boolArray: Array<boolean> = [true,false,true]
+  const boolArray: GenericArray<boolean> = [true, false, true];
 
+  const user: GenericArray<{ name: string; age: number }> = [
+    {
+      name: "chowa",
+      age: 23,
+    },
+    {
+      name: "afrin",
+      age: 26,
+    },
+  ];
 
-    const user: = [
-        {
-            name:'chowa',
-            age:23
-        },
-        {
-            name:'afrin',
-            age:26
-        },
-    ]
+  // generic tuple
+  type GenericTuple<X, Y> = [X, Y];
+  const manush: GenericTuple<string, string> = ["Mr. X", "Mr. Y"];
+  const UserWithID: GenericTuple<number, { name: string; email: string }> = [
+    1234,
+    { name: "chowa", email: "a@gmail.com" },
+  ];
 
-    // 
+  //
 }
